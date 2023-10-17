2ae8d90ecedba794f930048dff434f3e419f47a7
Kristians <murdskristians@gmail.com>
2023-10-17 20:25:45 +0300

    finished mobile frontend

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
     </div>
 
     <div class="green-block" id="green-block-1">
-        <p id="green-block-1-title" class="green-block-element">The Future of Farming</p>
+        <p id="green-block-1-title" class="green-block-element green-block-title">The Future of Farming</p>
 
         <p id="green-block-1-chapter-1" class="green-block-1-chapter green-block-element">
             Our vertical hydroponic farm solutions maximize growing space while minimizing energy and water usage.
@@ -54,7 +54,7 @@
             <img id="slider-2-img-1" class="slides" src="/images/slider-2-img-1.jpeg" alt="slide1.png">
         </div>
         <div id="slide-2-container-2" class="slide-containers" style="display: none">
-            <="slides" src="id="slider-2-img-2" class="slides" src="/images/slider-2-img-2.webp" alt="slide2.png">
+            <img id="slider-2-img-2" class="slides" src="/images/slider-2-img-2.jpeg" alt="slide1.png">
         </div>
         <div id="slide-2-container-3" class="slide-containers" style="display: none">
             <img id="slider-2-img-3" class="slides" src="/images/slider-2-img-3.webp" alt="slide3.png">
@@ -62,18 +62,18 @@
     </div>
 
     <div class="green-block" id="green-block-2">
-        <p id="green-block-2-title" class="green-block-1-element">Nutrient-Dense Plants</p>
+        <p id="green-block-2-title" class="green-block-2-element green-block-title">Nutrient-Dense Plants</p>
 
-        <p id="green-block-2-subtitle-1" class="green-block-element">Ultra-Fresh Produce</p>
-        <p id="green-block-2-chapter-1" class="green-block-1-chapter green-block-element">
+        <p id="green-block-2-subtitle-1" class="green-block-2-element green-block-subtitle">Ultra-Fresh Produce</p>
+        <p id="green-block-2-chapter-1" class="green-block-1-chapter green-block-2-element">
             According to a study from the University of California - Davis, most vegetables lose up to 55% of their
             nutrients within a week of harvest.
             The VHF enables you to harvest your plants just before they’re sold, ensuring maximum freshness and nutrient
             density.
         </p>
 
-        <p id="green-block-2-subtitle-2" class="green-block-element">Pesticide-Free Plants</p>
-        <p id="green-block-2-chapter-2" class="green-block-1-chapter green-block-element">
+        <p id="green-block-2-subtitle-2" class="green-block-2-element green-block-subtitle">Pesticide-Free Plants</p>
+        <p id="green-block-2-chapter-2" class="green-block-1-chapter green-block-2-element">
             The VHF provides a fully controlled, enclosed growing environment protected from pests and pathogens.
             This reduces and can eliminate entirely the need for pesticides, allowing you to deliver clean, untainted
             produce
@@ -97,17 +97,17 @@
 
 
     <div class="green-block" id="green-block-3">
-        <p id="green-block-3-title-1" class="green-block-element">Stay in Control</p>
-        <p id="green-block-3-title-2" class="green-block-element">Year-Round</p>
+        <p id="green-block-3-title-1" class="green-block-element green-block-title">Stay in Control</p>
+        <p id="green-block-3-title-2" class="green-block-element green-block-title">Year-Round</p>
 
-        <p id="green-block-3-subtitle-1" class="green-block-element">Full Spectrum LED Lighting</p>
+        <p id="green-block-3-subtitle-1" class="green-block-element green-block-subtitle">Full Spectrum LED Lighting</p>
         <p id="green-block-3-chapter-1" class="green-block-1-chapter green-block-element">
             Our high output LEDs mimic the sun and give your plants the full spectrum of light. Other systems only
             provide red and blue light,
             and the difference in yields is significant. The VHF lights help you grow larger, heavier plants with higher
             nutrient densities.
         </p>
-        <p id="green-block-3-subtitle-1" class="green-block-element">Climate Control</p>
+        <p id="green-block-3-subtitle-2" class="green-block-element green-block-subtitle">Climate Control</p>
         <p id="green-block-3-chapter-2" class="green-block-1-chapter green-block-element">
             The VHF allows you to dial in your temperatures, humidity requirements, and lighting schedules.
             Depending on your location and time of year, tap into the outside air to use less heating and air
@@ -132,15 +132,15 @@
 
 
     <div class="green-block" id="green-block-4">
-        <p id="green-block-4-title-1" class="green-block-element">Grow Nearly 8.000</p>
-        <p id="green-block-4-title-2" class="green-block-element">Plants at Once</p>
+        <p id="green-block-4-title-1" class="green-block-element green-block-title">Grow Nearly 8.000</p>
+        <p id="green-block-4-title-2" class="green-block-element green-block-title">Plants at Once</p>
 
-        <p id="green-block-4-subtitle-1" class="green-block-element">Movable Grow Walls</p>
+        <p id="green-block-4-subtitle-1" class="green-block-element green-block-subtitle">Movable Grow Walls</p>
         <p id="green-block-4-chapter-1" class="green-block-4-chapter green-block-4-element">
             Create space where you need it - moving grow walls create wide hallways allowing
             you to inspect your plants, transplant, and harvest without feeling cramped.
         </p>
-        <p id="green-block-4-subtitle-1" class="green-block-element">Space Efficiency</p>
+        <p id="green-block-4-subtitle-1" class="green-block-element green-block-subtitle">Space Efficiency</p>
         <p id="green-block-4-chapter-2" class="green-block-4-chapter green-block-4-element">
             Grow tubes achieve higher weekly harvest numbers than our competitors. Over the course of a year,
             the higher yields and heavier plants you get with the VHF will really add up.
