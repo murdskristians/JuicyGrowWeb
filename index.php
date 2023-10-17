4c75cf80f0b1cae0c1263e90e0bf3bd7ced097ba
Kristians <murdskristians@gmail.com>
2023-10-17 14:52:06 +0300

    making green block

--- index.php
+++ index.php
@@ -2,13 +2,41 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>hello</title>
+    <title>JuicyGrow</title>
+    <link rel="stylesheet" href="/styles/styles.css">
+
 </head>
 <body>
 
-<h1>hellooooo</h1>
+    <div id="screen-sides-picture"> </div>
 
+    <div id="logo-container">
+        <img id="logo" src="/images/logo.png" alt="logo JuicyGrow">
+    </div>
 
+    <div id="language-container">
+        <a href="/index.php?lang=en">EN</a>
+        <a href="/index.php?lang=de">DE</a>
+    </div>
+
+    <div id="slide-wrapper">
+        <div id="slide-container-1">
+            <img id="slide1" class="slides" src="/images/slider-img-1.jpg" alt="slide1.png">
+        </div>
+        <div id="slide-container-2" style="display: none">
+            <img id="slide2" class="slides" src="/images/slider-img-2.webp" alt="slide2.png" >
+        </div>
+    </div>
+
+    <div class="green-block">
+        <h1>The Future of Farming</h1>
+        <p></p>
+        <p>Our vertical hydroponic farm solutions maximize growing space while minimizing energy and water usage.</p>
+        <p></p>
+        <p>Combining patented vertical farming technology our ultra-efficient farms transport easily, can be stacked,
+            and are solar capable. Join us in decentralizing agriculture and reducing carbon emissions while helping to feed the world.
+        </p>
+    </div>
 
 </body>
 </html>
