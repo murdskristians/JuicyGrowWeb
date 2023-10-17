15e6daaad0d576a80b869389b9046d8dd911c793
Kristians <murdskristians@gmail.com>
2023-10-17 18:22:58 +0300

    uploaded the rest slides pictures

--- index.php
+++ index.php
@@ -8,35 +8,130 @@
 </head>
 <body>
 
-    <div id="screen-sides-picture"> </div>
 
-    <div id="logo-container">
-        <img id="logo" src="/images/logo.png" alt="logo JuicyGrow">
-    </div>
+<div id="screen-sides-picture"></div>
 
-    <div id="language-container">
-        <a href="/index.php?lang=en">EN</a>
-        <a href="/index.php?lang=de">DE</a>
-    </div>
+<div id="logo-container">
+    <img id="logo" src="/images/logo.png" alt="logo JuicyGrow">
+</div>
 
+
+<div id="language-container">
+    <a href="/index.php?lang=en">EN</a>
+    <a href="/index.php?lang=de">DE</a>
+</div>
+
+
+<div class="slider-with-text-container">
     <div id="slide-wrapper">
-        <div id="slide-container-1">
-            <img id="slide1" class="slides" src="/images/slider-img-1.jpg" alt="slide1.png">
+        <div id="slide-1-container-1" class="slide-containers">
+            <img id="slider-1-img-1" class="slides" src="/images/slider-1-img-1.jpg" alt="slide1.png">
         </div>
-        <div id="slide-container-2" style="display: none">
-            <img id="slide2" class="slides" src="/images/slider-img-2.webp" alt="slide2.png" >
+        <div id="slide-1-container-2" class="slide-containers" style="display: none">
+            <img id="slider-1-img-2" class="slides" src="/images/slider-1-img-2.webp" alt="slide2.png">
         </div>
     </div>
 
     <div class="green-block" id="green-block-1">
         <p id="green-block-1-title" class="green-block-1-element">The Future of Farming</p>
 
-        <p id="green-block-1-chapter-1" class="green-block-1-chapter green-block-1-element">Our vertical hydroponic farm solutions maximize growing space while minimizing energy and water usage.</p>
+        <p id="green-block-1-chapter-1" class="green-block-1-chapter green-block-1-element">
+            Our vertical hydroponic farm solutions maximize growing space while minimizing energy and water usage.
+        </p>
 
-        <p id="green-block-1-chapter-2" class="green-block-1-chapter green-block-1-element">Combining patented vertical farming technology our ultra-efficient farms transport easily, can be stacked,
-            and are solar capable. Join us in decentralizing agriculture and reducing carbon emissions while helping to feed the world.
+        <p id="green-block-1-chapter-2" class="green-block-1-chapter green-block-1-element">
+            Combining patented vertical farming technology our ultra-efficient farms transport easily, can be stacked,
+            and are solar capable. Join us in decentralizing agriculture and reducing carbon emissions while helping to
+            feed the world.
         </p>
     </div>
+</div>
+
+
+<div class="slider-with-text-container">
+    <div id="slide-wrapper">
+        <div id="slide-2-container-1" class="slide-containers">
+            <img id="slider-2-img-1" class="slides" src="/images/slider-2-img-1.jpeg" alt="slide1.png">
+        </div>
+        <div id="slide-2-container-2" class="slide-containers" style="display: none">
+            <="slides" src="id="slider-2-img-2" class="slides" src="/images/slider-2-img-2.webp" alt="slide2.png">
+        </div>
+        <div id="slide-2-container-3" class="slide-containers" style="display: none">
+            <img id="slider-2-img-3" class="slides" src="/images/slider-2-img-3.webp" alt="slide3.png">
+        </div>
+    </div>
+
+    <div class="green-block" id="green-block-1">
+        <p id="green-block-1-title" class="green-block-1-element">The Future of Farming</p>
+
+        <p id="green-block-1-chapter-1" class="green-block-1-chapter green-block-1-element">
+            Our vertical hydroponic farm solutions maximize growing space while minimizing energy and water usage.
+        </p>
+
+        <p id="green-block-1-chapter-2" class="green-block-1-chapter green-block-1-element">
+            Combining patented vertical farming technology our ultra-efficient farms transport easily, can be stacked,
+            and are solar capable. Join us in decentralizing agriculture and reducing carbon emissions while helping to
+            feed the world.
+        </p>
+    </div>
+</div>
+
+<div class="slider-with-text-container">
+    <div id="slide-wrapper">
+        <div id="slide-3-container-1" class="slide-containers">
+            <img id="slider-3-img-1" class="slides" src="/images/slider-3-img-1.webp" alt="slide1.png">
+        </div>
+        <div id="slide-3-container-2" class="slide-containers" style="display: none">
+            <img id="slider-3-img-2" class="slides" src="/images/slider-3-img-2.webp" alt="slide2.png">
+        </div>
+        <div id="slide-3-container-3" class="slide-containers" style="display: none">
+            <img id="slider-3-img-3" class="slides" src="/images/slider-3-img-3.webp" alt="slide3.png">
+        </div>
+    </div>
+
+    <div class="green-block" id="green-block-1">
+        <p id="green-block-1-title" class="green-block-1-element">The Future of Farming</p>
+
+        <p id="green-block-1-chapter-1" class="green-block-1-chapter green-block-1-element">
+            Our vertical hydroponic farm solutions maximize growing space while minimizing energy and water usage.
+        </p>
+
+        <p id="green-block-1-chapter-2" class="green-block-1-chapter green-block-1-element">
+            Combining patented vertical farming technology our ultra-efficient farms transport easily, can be stacked,
+            and are solar capable. Join us in decentralizing agriculture and reducing carbon emissions while helping to
+            feed the world.
+        </p>
+    </div>
+</div>
+
+
+<div class="slider-with-text-container">
+    <div id="slide-wrapper">
+        <div id="slide-4-container-1" class="slide-containers">
+            <img id="slider-4-img-1" class="slides" src="/images/slider-4-img-1.webp" alt="slide1.png">
+        </div>
+        <div id="slide-4-container-2" class="slide-containers" style="display: none">
+            <img id="slider-4-img-2" class="slides" src="/images/slider-4-img-2.webp" alt="slide2.png">
+        </div>
+        <div id="slide-4-container-3" class="slide-containers" style="display: none">
+            <img id="slider-4-img-3" class="slides" src="/images/slider-4-img-3.webp" alt="slide3.png">
+        </div>
+    </div>
+
+    <div class="green-block" id="green-block-1">
+        <p id="green-block-1-title" class="green-block-1-element">The Future of Farming</p>
+
+        <p id="green-block-1-chapter-1" class="green-block-1-chapter green-block-1-element">
+            Our vertical hydroponic farm solutions maximize growing space while minimizing energy and water usage.
+        </p>
+
+        <p id="green-block-1-chapter-2" class="green-block-1-chapter green-block-1-element">
+            Combining patented vertical farming technology our ultra-efficient farms transport easily, can be stacked,
+            and are solar capable. Join us in decentralizing agriculture and reducing carbon emissions while helping to
+            feed the world.
+        </p>
+    </div>
+</div>
 
 </body>
 </html>
