c29633912b18584f26e1c69e16c2db05dc416026
Kristians <murdskristians@gmail.com>
2023-10-17 15:18:47 +0300

    finished first page on mobile

--- index.php
+++ index.php
@@ -28,12 +28,12 @@
         </div>
     </div>
 
-    <div class="green-block">
-        <h1>The Future of Farming</h1>
-        <p></p>
-        <p>Our vertical hydroponic farm solutions maximize growing space while minimizing energy and water usage.</p>
-        <p></p>
-        <p>Combining patented vertical farming technology our ultra-efficient farms transport easily, can be stacked,
+    <div class="green-block" id="green-block-1">
+        <p id="green-block-1-title" class="green-block-1-element">The Future of Farming</p>
+
+        <p id="green-block-1-chapter-1" class="green-block-1-chapter green-block-1-element">Our vertical hydroponic farm solutions maximize growing space while minimizing energy and water usage.</p>
+
+        <p id="green-block-1-chapter-2" class="green-block-1-chapter green-block-1-element">Combining patented vertical farming technology our ultra-efficient farms transport easily, can be stacked,
             and are solar capable. Join us in decentralizing agriculture and reducing carbon emissions while helping to feed the world.
         </p>
     </div>
